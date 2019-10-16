e29a85d93a8ec0fe535fc747d23096e0a7432da4
Gabriel Trabanco Llano <gtrabanco@fwok.org>
2019-10-16 23:46:39 +0300

    Fixed error that causes the speed were not changed

--- app/speedcontrol.php
+++ app/speedcontrol.php
@@ -21,7 +21,8 @@ $attr_max_speed_total_data     = getenv('MAX_SPEED_TOTAL_DATA');
 $attr_reduced_speed_usergroup = getenv('REDUCED_SPEED_USERGROUP');
 $attr_speed_reset_period = getenv('REDUCED_SPEED_RESET_PERIOD');
 
-$attr_prevail_unifi = getenv('PREVAIL_UNIFI');
+$prevail_unifi = (bool)getenv('PREVAIL_UNIFI');
+
 
 $reply_attribute_fixed_speed_usergroup = getenv('SPEED_USERGROUP');
 
@@ -45,52 +46,17 @@ $unifi_usegroups = $unifi_connection->list_usergroups();
 $device_unifi_info = $unifi_connection->stat_client($device_mac);
 $device_unifi_info = is_array($device_unifi_info) && count($device_unifi_info) > 0 ? $device_unifi_info[0]:array();
 
-//$results          = $unifi_connection->unblock_sta($my_mac); // unblock / block device
-//$results2         = $unifi_connection->reconnect_sta($my_mac); // reconnect a device
-// To know the client usergroup:
-//  stat_client($mac)->usergroup_id
-// If it is empty user has the default group, we can also set the default group using
-// empty group_id when setting device speed
-//
-// We will also use stat_client() because we do not want a list of all connected devices
-// we just want the known mac address client
-// If the user has a group different than default and is not max speed or reduced speed
-// group from freeradius configuration we will finish because maybe the admin set
-// a custom group for the device. We do not want to modifi Unifi behaviour.
-
 // If we dont have device we finish
 if (!$device_unifi_info) {
-    //die('Not device to modify the speed: ' . $device_mac);
+    //die('Not device to modify the speed: ' . $device_mac . "\n");
 
     echo "ok";
     exit(0);
 }
 
 
-// <set_radius_user_in_unifi>
-/* Commented because unifi can do this by its own right now
-// Set the user the user that is using the device in the unifi
-// First is there is no name, we will use the hostname after the user
-$previousDeviceName = (isset($device_unifi_info->name)?$device_unifi_info->name:$device_unifi_info->hostname);
-
-// The format to set the user and the name we have already assigned in unifi if there is or
-// the hostname if not. Note if there is any other user that use the device we will get a
-// big name with all users in unifi. This is not a good advantage...
-$nameFmt = "| %s |"; // To comparison
-$finalNameFmt = $nameFmt . " - %s"; // How the name will look like in unifi
-
-$fmtUsername = preg_quote(sprintf($nameFmt, $FRUsername)); // Comparison Name
-
-if (!preg_match("/(${fmtUsername})+/is", $previousDeviceName)) { // The user was no set previously
-    $unifi_connection -> set_sta_name($device_unifi_info->_id, sprintf($finalNameFmt, $FRUsername, $previousDeviceName));
-}
-//*/
-// </set_radius_user_in_unifi>
-
-
 // <fixed_usergroup_by_radius>
 // If the user has a specific usergroup for speed in Radius apply it and end
-//$attr_speed_usergroup = filter_array_key_value($user_reply_attributes, $attr, $attr_name);
 $attr_speed_usergroup = filter_array_key_value($user_reply_attributes, 'attribute', $reply_attribute_fixed_speed_usergroup);
 
 if (count($attr_speed_usergroup) > 0) {
@@ -103,6 +69,8 @@ if (count($attr_speed_usergroup) > 0) {
     $unifi_user_usergroup = filter_array_object_value($unifi_usegroups, 'name', ($attr_speed_usergroup[0]['value']));
     $unifi_user_usergroup = count($unifi_user_usergroup) > 0? $unifi_user_usergroup[0]: $unifi_user_usergroup;
     $group_id = is_object($unifi_user_usergroup) && isset($unifi_user_usergroup->_id) ?$unifi_user_usergroup->_id : null;
+
+    echo "\nThe user has specific group id: $group_id\n";
 }
 // </fixed_usergroup_by_radius>
 
@@ -116,6 +84,7 @@ if (count($attr_speed_usergroup) > 0) {
 $reduced_speed_group = filter_array_key_value($user_reply_attributes, 'attribute', $attr_reduced_speed_usergroup);
 $speedcontrol_period = filter_array_key_value($user_reply_attributes, 'attribute', $attr_speed_reset_period);
 
+
 // We will check if there is the minimum configuration for apply a reduced speed which is
 // a group of reduced speed and period
 if (isset($reduced_speed_group) && count($reduced_speed_group) < 1 && isset($speedcontrol_period) && count($speedcontrol_period) < 1) {
@@ -127,18 +96,16 @@ if (isset($reduced_speed_group) && count($reduced_speed_group) < 1 && isset($spe
 // </check_if_user_has_reduced_speed_usergroup>
 
 // <check_if_user_reduced_speed_usergroup_exists_in_unifi_and_limits>
+
 // Check if the reduced speed group exists in Unifi
 // First we get the values
 $usegroup_reduced_speed_name = isset($reduced_speed_group) && isset($reduced_speed_group[0])?$reduced_speed_group[0]['value']:null;
 $unifi_reduced_usergroup = !empty($usegroup_reduced_speed_name)?filter_array_object_value($unifi_usegroups, 'name', $usegroup_reduced_speed_name):array();
 
-// If there is reduced speed usergroup:
-// Depending if Customunifi-Prevail-Unifi is defined and a value considered true by PHP then
+// If we specified in env that unifi prevail over all is defined and a value considered true by PHP then
 // If the admin has defined a custom group for the device in the unifi we won't change
 // the speed. But if the group is the reduced speed we will continue the process.
-if(count($unifi_reduced_usergroup) > 1
-  && isset($attr_prevail_unifi)
-  && $attr_prevail_unifi
+if($prevail_unifi
   && isset($device_unifi_info->usergroup_id)
   && strlen($device_unifi_info->usergroup_id) > 0
   && $device_unifi_info->usergroup_id !== $unifi_reduced_usergroup[0]->_id) { // This last check if the
@@ -147,9 +114,9 @@ if(count($unifi_reduced_usergroup) > 1
                                     // user must be in the default group. Because maybe we are in a
                                     // different billing period
     //die('The user has a custom group so we will not change the speed group for the device.');
-    $group_id = $device_unifi_info->usergroup_id;
+    $group_id = $device_unifi_info->usergroup_id; //Assigning the current value to the device
 
-} else if (count($unifi_reduced_usergroup) > 1) {
+} else if (count($unifi_reduced_usergroup) > 0) {
     // If there is any reduced group we can check
     // if we must reduce the speed of the user
 
@@ -184,11 +151,12 @@ $unifi_connection->set_usergroup($user_id, $group_id);
 $pdo = null; // Good bye MySQL!
 echo "ok";
 exit(0); // No one can include this file, it will be used as a binary
+
 /**
- * Este script debe estar en la sección "accounting {}". Suponiendo que php esté en
- * "/usr/bin/php" y el script en 
+ * This script must be in "accounting {}" section. If we suppose that php is
+ * in "/usr/bin/php" and the script in 
  * "/var/www/daloradius/contrib/scripts/maintenance/vendor/unifi-speedcontrol/main.php"
- * Debemos agregar la siguiente configuración:
+ * We must add this in our accounting FR section:
  * 
  *    update control {
  *        Tmp-Integer-0 = `/usr/bin/php -f /var/www/daloradius/contrib/scripts/maintenance/vendor/unifi-freeradius-scripts/app/speedcontrol.php`
@@ -196,6 +164,6 @@ exit(0); // No one can include this file, it will be used as a binary
  * 
  * 
  * 
- * Ayuda exec de Freeradius:
+ * Help with FR exec module:
  * https://networkradius.com/doc/3.0.10/raddb/mods-available/exec.html
  */

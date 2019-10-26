0f76892cf63e6bf1f0d5e7e066dc7d67fba7d953
Gabriel Trabanco Llano <gtrabanco@fwok.org>
2019-10-26 12:40:42 +0300

    Added a new feature to reconnect all clients in a given network

---
+++ app/reconnect_all_sta.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * This scripts is thinked to be a crontab every X minutes.
+ * This script will reconnect all clients connected to specified
+ * network.
+ * 
+ * Usage:
+ *      /usr/bin/php reconnect_all_sta.php NETWORK
+ * 
+ * 
+ *          gabi.io
+ * 
+ * @author Gabriel Trabanco Llano <gtrabanco@fwok.org>
+ */
+
+
+set_time_limit(30); //Shoul be enought, it should take no more
+            //than 2/3 seconds reconnect all clients
+
+set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__);
+
+require_once('vendor/autoload.php');
+require_once('library/functions.php');
+
+$dotenv = Dotenv\Dotenv::create(join(array(__DIR__, 'config'), DIRECTORY_SEPARATOR));
+$dotenv->load();
+
+// Get necessary params if not print the help
+if ($argc < 2) {
+
+    echo "The usage is\n";
+    echo "\t" .$argv[0] ." <network>\n";
+    exit;
+} else {
+    $network = $argv[1];
+}
+
+// Connect to unifi
+$unifi_connection = loginUnifi();
+
+// The lisf of the devices
+$clients = $unifi_connection->list_clients();
+
+// Reconnect all clients in the network
+array_filter($clients, function ($client) use ($network) {
+    
+
+    $client_essid   = isset($client->essid)?strtolower(trim($client->essid)):'';
+    $client_network = isset($client->network)?strtolower(trim($client->network)):'';
+
+    if( strlen($client_essid) > 0 && $client_essid !== $network
+        || strlen($client_network) > 0 && $client_network !== $network) {
+            $unifi_connection->reconnect_sta($client->mac);
+    }
+});
